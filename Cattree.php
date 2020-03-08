e6d1c505e43e2118632ab35a313af8cb8546cb3d
Only One <oncwnuOuJTEu2mnbnKdwIydvr3No@git.weixin.qq.com>
2020-03-08 22:09:08 +0800

    fisrt commit

---
+++ Cattree.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Lmonkey\Cattree;
+
+/**
+ * @method static Cattree get(string $route, Callable $callback)
+ * @method static Cattree post(string $route, Callable $callback)
+ * @method static Cattree put(string $route, Callable $callback)
+ * @method static Cattree delete(string $route, Callable $callback)
+ * @method static Cattree options(string $route, Callable $callback)
+ * @method static Cattree head(string $route, Callable $callback)
+ */
+class Cattree {
+ 
+    public static $error_callback;
+
+    
+
+}
---
+++ src/CpsTplExample.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Lmonkey\Cattree;
+
+class CpsTplExample {
+
+    public function show() {
+        return "Hi I'm magina, this is my template of composer project!";
+    }
+    
+}
